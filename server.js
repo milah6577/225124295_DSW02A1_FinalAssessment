4e2b56f7c602e3d9ffb6670866356aa157a22c46
milah6577 <lindomathonsi25@gmail.com>
2026-06-02 12:13:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.use(express.json()); // Essential for receiving JSON
+app.use(express.json()); 
 
 mongoose.connect("mongodb://127.0.0.1:27017/helpdeskDB");
 
